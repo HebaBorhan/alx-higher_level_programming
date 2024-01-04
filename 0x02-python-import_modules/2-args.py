7409cfe826acb570547698f2e57e6b22b975854e
HebaZaki <h_borhan@hotmail.com>
2024-01-04 22:35:25 +0200

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,12 @@ if __name__ == '__main__':
 
     if (len(sys.argv)-1) == 1:
         print("1 argument:\n1: {}".format(sys.argv[1]))
-    
+
     elif (len(sys.argv)-1) == 0:
-                print("0 arguments.\n")
+        print("0 arguments.\n")
+
     else:
         print("{} arguments:".format((len(sys.argv)-1)))
 
-        for i in range (len(sys.argv)-1):
+        for i in range(len(sys.argv)-1):
             print("{}: {}".format((i), (sys.argv[i])))
