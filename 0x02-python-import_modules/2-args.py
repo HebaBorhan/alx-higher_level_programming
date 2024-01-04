5f901511a191ed3a5007cf00419073a2a6065aac
HebaZaki <h_borhan@hotmail.com>
2024-01-04 22:31:33 +0200

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+
+    if (len(sys.argv)-1) == 1:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    
+    elif (len(sys.argv)-1) == 0:
+                print("0 arguments.\n")
+    else:
+        print("{} arguments:".format((len(sys.argv)-1))
+
+        for i in range (len(sys.argv)-1):
+            print("{}: {}".format((i), (sys.argv[i])))
