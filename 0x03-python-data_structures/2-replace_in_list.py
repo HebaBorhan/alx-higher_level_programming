dbf9d6facd70a391c0529fc663a45bb9f55c3566
HebaZaki <h_borhan@hotmail.com>
2024-01-07 01:07:02 +0200

    replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,8 @@ def replace_in_list(my_list, idx, element):
         return my_list
 
     else:
-        new_list = my_list
-        new_element = new_list[idx]
+        temp = my_list
+        new_element = 9
+        temp[idx] = new_element
+        new_list = temp
         return new_list
