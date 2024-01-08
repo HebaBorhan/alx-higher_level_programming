69182388a33f87a9d6f1fd90090e332a3856a277
HebaZaki <h_borhan@hotmail.com>
2024-01-08 18:17:51 +0200

    remove letter from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if len(my_string) == 0:
-        return None
-    else:
+    if my_string:
         new_string = (my_string.translate({ord(i): None for i in 'cC'}))
         return new_string
