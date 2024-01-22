9435b99bb4451bfe4961b2db79b42af88a0e15f7
HebaZaki <h_borhan@hotmail.com>
2024-01-22 23:33:44 +0200

    safe print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        print("Inside result: {}".format(result))
+    except ZeroDivisionError:
+        result = None
+        print("Inside result: None")
+    finally:
+        return result
