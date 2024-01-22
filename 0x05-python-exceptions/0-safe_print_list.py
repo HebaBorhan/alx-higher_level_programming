82a75d2aeb448fe35cc686f46b6f5e01f7443918
HebaZaki <h_borhan@hotmail.com>
2024-01-22 16:49:23 +0200

    safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end='')
+    
+    except TypeError:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end='')
+    print("\nnb_print: {:d}".format(x))
