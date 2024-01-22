40b18f9cd611c82c21d45ff8ca181304280b7fd2
HebaZaki <h_borhan@hotmail.com>
2024-01-22 22:58:41 +0200

    raise exception msg

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    raise NameError("C is fun")
+    try:
+        raise NameError("C is fun")
+
+    except NameError as ne:
+        print(ne)
