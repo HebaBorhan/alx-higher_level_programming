80bf87523abc15349d5cca6df94f4ae076ca8e60
HebaZaki <h_borhan@hotmail.com>
2024-01-23 22:53:59 +0200

    square position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""defining Square by Size module."""
+
+
+class Square:
+    """class Square that defines a square by Size."""
+
+    def __init__(self, size=0):
+        """Initialize the Square class with a given 'size' argument."""
+        self._size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self._size = value
+
+    def area(self):
+        """Returns the area of the square."""
+        return self._size ** 2
+
+    def my_print(self):
+        """Prints out the size of the square as '#'."""
+        if self._size == 0:
+            print("")
+        else:
+            for value in range(self._size):
+                print("#" * self._size)
