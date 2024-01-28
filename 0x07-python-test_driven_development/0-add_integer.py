07a4b002f2818c3000deb0b0de6c54386810bfd2
HebaZaki <h_borhan@hotmail.com>
2024-01-28 21:08:10 +0200

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
 defining add module.
-it has 1 function that returns the sum of two integer or floats. adds two numbers together and returns the result.
+it has 1 function that returns the sum of two integer or floats.
 It also includes a docstring that explains what this function does.
 """
 
+
 def add_integer(a, b=98):
-    """ 
-    If only one argument is provided, it will be used as the first value, and The default value for `b` is 98.
+    """If only one argument is provided, it will be used as the first value,
+    and The default value for `b` is 98.
     """
     if isinstance(a, float):
         a = int(a)
