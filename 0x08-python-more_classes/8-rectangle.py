7555845d62eec1fde6835cade56279030cb234cb
HebaZaki <h_borhan@hotmail.com>
2024-01-30 01:41:23 +0200

    static

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
     class Rectangle that defines a rectangle by width and height
     """
     number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """Initialize Rectangle with width and height
@@ -16,6 +17,7 @@ class Rectangle:
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
+        Rectangle.print_symbol
 
     @property
     def width(self):
@@ -55,12 +57,14 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            rect = ("#" * self.__width + '\n') * self.__height
+            rect = (str(self.print_symbol) * self.__width
+                    + '\n') * self.__height
             # Remove the extra newline character at the end
             return rect[:-1]
 
     def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(str(self.__width),
+                                          str(self.__height))
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
