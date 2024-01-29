7dc0c6c5f2423638058c02cd2a1161b8f93da434
HebaZaki <h_borhan@hotmail.com>
2024-01-30 01:37:25 +0200

    print symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -54,16 +54,16 @@ class Rectangle:
             return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            rect = (str(self.print_symbol) * self.width
-                    + '\n') * self.height
+            rect = (str(self.print_symbol) * self.__width
+                    + '\n') * self.__height
             # Remove the extra newline character at the end
             return rect[:-1]
 
     def __repr__(self):
-        return "Rectangle({}, {})".format(str(self.width), str(self.height))
+        return "Rectangle({}, {})".format(str(self.__width), str(self.__height))
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
