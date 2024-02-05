b1cd5846378d4b67c402c06ecee2d86193616e58
HebaZaki <h_borhan@hotmail.com>
2024-02-06 00:34:11 +0200

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""defining BaseGeometry module."""
+
+
+class BaseGeometry:
+    """class BaseGeometry."""
+
+    def __init__(self):
+        """Initialize BaseGeometry class."""
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Square(BaseGeometry):
+    """subclass Square."""
+    def __init__(self, size):
+        """Initialize subclass Square."""
+        super().__init__()
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
+
+    def area(self):
+        return self.__size **2
