ccdac9cd6eeb02ba71cc966f72bb0e22ee706625
HebaZaki <h_borhan@hotmail.com>
2024-02-05 23:36:26 +0200

    geometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defining BaseGeometry module."""
+
+
+class BaseGeometry:
+    """class BaseGeometry."""
+
+    def __init__(self):
+        """Initialize the Square class with a given 'size' argument."""
+
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+        
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
