af34bdaff566819dbc3f0c88c04d3fd91d97b2e9
HebaZaki <h_borhan@hotmail.com>
2024-02-05 21:51:36 +0200

    print list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,13 +3,11 @@
 module of List Class and MyList subclass
 """
 
-    
+
 class MyList(list):
     """derived class MyList"""
     def __init__(self):
         super().__init__()
-    
-    def print_sorted(self):
-        print(sorted(self)) 
 
-        
+    def print_sorted(self):
+        print(sorted(self))
