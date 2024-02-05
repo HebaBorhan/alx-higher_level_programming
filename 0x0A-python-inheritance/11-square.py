eaf4fdb04805b11ed4685288ef3eab6446e72170
HebaZaki <h_borhan@hotmail.com>
2024-02-06 00:38:02 +0200

    square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -29,7 +29,7 @@ class Square(BaseGeometry):
         self.__size = size
 
     def __str__(self):
-        return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
 
     def area(self):
         return self.__size ** 2
