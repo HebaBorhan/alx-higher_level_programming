802331757ed415c02697569586174c5d2181c6c9
HebaZaki <h_borhan@hotmail.com>
2024-02-05 21:47:04 +0200

    print list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,17 +3,13 @@
 module of List Class and MyList subclass
 """
 
-
-class list():
-    """list class"""
-    def __init__(self, iterable=None):
-        self.data = [] if iterable is None else list(iterable)
     
-
 class MyList(list):
     """derived class MyList"""
+    def __init__(self):
+        super().__init__()
+    
     def print_sorted(self):
-        """print elements in sorted order"""
-        print(" ".join([str(i) for i in sorted(set(self.data
-                                                   ))]))
+        print(sorted(self)) 
+
         
