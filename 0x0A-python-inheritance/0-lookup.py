f91ef8765ef176bbf7e57db8243ac1ed09a43e74
HebaZaki <h_borhan@hotmail.com>
2024-02-05 14:17:25 +0200

    dir function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module to lookup object
+"""
+
+
+def lookup(obj):
+    """
+    function returns list of available attributes and methods of an object
+    """
+    return dir(obj)
