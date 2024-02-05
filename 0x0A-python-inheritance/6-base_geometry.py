5dc693da540394659318a7969b139a72320ba02b
HebaZaki <h_borhan@hotmail.com>
2024-02-05 23:06:59 +0200

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defining BaseGeometry module."""
+
+
+class BaseGeometry:
+    """class BaseGeometry."""
+
+    def __init__(self):
+        """Initialize the Square class with a given 'size' argument."""
+
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
