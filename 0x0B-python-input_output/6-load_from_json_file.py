16591f3cad3f6c36a86db5cd1f14391284b085ee
HebaZaki <h_borhan@hotmail.com>
2024-02-06 20:07:23 +0200

    load from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""JSON representation module"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a "JSON file"."""
+    with open(filename, "w", encoding="utf-8") as f:
+        deserialized_data = json.load(f)
+        return (deserialized_data)
