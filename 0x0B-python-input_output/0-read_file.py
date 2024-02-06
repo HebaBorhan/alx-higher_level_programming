98635c858f1527b9cb2eee51dbe0fe85e4233f1c
HebaZaki <h_borhan@hotmail.com>
2024-02-06 17:38:23 +0200

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""read file module"""
 
 
 def read_file(filename=""):
+    """Read a text file"""
     with open(filename, encoding="utf-8") as f:
-        f.read()
+        print(f.read(), end="")
