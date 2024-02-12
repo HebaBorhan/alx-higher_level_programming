c19a842cd9302be7b71543d0923a89bd55d8ec07
HebaZaki <h_borhan@hotmail.com>
2024-02-12 22:04:52 +0200

    models Files

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,28 +9,33 @@ class Square(Rectangle):
         """initializes function"""
         super().__init__(size, size, x, y, id)
 
-
     @property
     def size(self):
+        """size of square"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """size setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
-    
+
     def display(self):
+        """Display the Square by #."""
         print('\n' * self.y, end="")
-        print(((" " * self.x ) + "#" * self.width + '\n') * self.width, end="")
+        print(((" " * self.x) + "#" * self.width + '\n') * self.width, end="")
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    
+        """printing Square string"""
+        return "[Square] ({}) {}/{} - {}".\
+            format(self.id, self.__x, self.__y, self.__width)
+
     def update(self, *args, **kwargs):
+        """Update the position and size of the Square."""
         if args:
             attr_names = ["id", "width", "height", "x", "y"]
             for i, arg in enumerate(args):
@@ -40,4 +45,5 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """Convert a Square object into a dictionary."""
         return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
