76d9e8ac67d4e8497ee6c4def3c96090fda810d4
HebaZaki <h_borhan@hotmail.com>
2024-03-12 21:26:03 +0200

    SQL intro

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- This script displays the average temp. by city ordered by temperature (desc.)
 
 SOURCE C:\Users\h_bor\Downloads\temperatures.sql
-SELECT city, AVG(value) AS avg_temp FROM temperatures ORDER BY value DESC;
+SELECT city, AVG((value * 9/5) + 32) AS avg_temp FROM temperatures ORDER BY avg_temp DESC;
