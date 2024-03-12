30f5ab710536a5b79ccf0f6d18b4f3f6c0ca4945
HebaZaki <h_borhan@hotmail.com>
2024-03-12 12:24:58 +0200

    SQL Intro

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,1 +1,12 @@
--- This script
+-- This script creates a table second_table
+
+CREATE TABLE if NOT EXISTS second_table(
+    id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    score INT NOT NULL);
+
+INSERT INTO second_table (id, name, score) VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
