d57322dd85fc0b9e4be14eea333fd65c66ad4da7
HebaZaki <h_borhan@hotmail.com>
2024-03-13 21:44:48 +0200

    states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- This script creates the database hbtn_0d_usa and the table states
+
+CREATE DATABASE if NOT EXISTS hbtn_0d_usa;
+CREATE TABLE if NOT EXISTS states(
+    id INT NOT NULL DEFAULT 1 UNIQUE,
+    name VARCHAR(256) NOT NULL);
