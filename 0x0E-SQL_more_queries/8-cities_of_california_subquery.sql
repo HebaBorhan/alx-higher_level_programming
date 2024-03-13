490fd54a0326737db5a47391270b7eca76ee7506
HebaZaki <h_borhan@hotmail.com>
2024-03-13 22:47:51 +0200

    cities of california subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- This script lists all the cities of California found in the database hbtn_0d_usa
 
+USE hbtn_0d_usa;
 SELECT cities.id, cities.name, state.id
         FROM cities
         WHERE state.id =        
