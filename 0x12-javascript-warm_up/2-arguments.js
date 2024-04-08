f07397b4318cde1f1fa9841686e85f8dd9830ceb
HebaZaki <h_borhan@hotmail.com>
2024-04-08 22:26:14 +0200

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const argvLen = process.argv.length;
 
 if (argvLen === 2) {
   console.log('No argument');
-} else if (argsLength === 3) {
+} else if (argvLen === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
