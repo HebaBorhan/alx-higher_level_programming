c8c8814931db0b6d3d52cb014aaf6500496e71c1
HebaZaki <h_borhan@hotmail.com>
2024-04-10 22:17:51 +0200

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -5,7 +5,7 @@ const my_aray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 let length = my_aray.length;
 
-while (i <= length)
+while (i < length)
 {
     console.log(my_aray[i]);
     i++;
