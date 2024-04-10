4b5b9229b71a6e22203c2ac368d168dc517c6ba6
HebaZaki <h_borhan@hotmail.com>
2024-04-10 22:17:24 +0200

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const my_aray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+let length = my_aray.length;
+
+while (i <= length)
+{
+    console.log(my_aray[i]);
+    i++;
+}
