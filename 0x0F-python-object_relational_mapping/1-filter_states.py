9e078ef80e4c268c40a6043aa6def45911b608e8
HebaZaki <h_borhan@hotmail.com>
2024-04-13 00:38:12 +0200

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N from database"""
+"""lists all states with name starting with N from database"""
 import MySQLdb
 import sys
 
@@ -13,7 +13,7 @@ if __name__ == '__main__':
                            charset="utf8")
     cur = conn.cursor()
     cur.execute(
-        "SELECT DISTINCT * FROM states WHERE NAME LIKE 'N%' ORDER BY id ASC")
+        "SELECT * FROM states WHERE NAME LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
