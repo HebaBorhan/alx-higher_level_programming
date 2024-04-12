2a7521a3771b499c221dcb60f192a0eee3b11aa9
HebaZaki <h_borhan@hotmail.com>
2024-04-12 23:00:21 +0200

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
                            passwd=sys.argv[2],
                            db=sys.argv[3],
                            charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
