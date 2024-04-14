90724281862ea9b75a5a7f2f8915b715b89e0fed
HebaZaki <h_borhan@hotmail.com>
2024-04-14 21:51:59 +0200

    my states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N from database"""
+"""displays all values in the states table where name matches argument"""
 import MySQLdb
 import sys
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print("{}".format(sys.argv[0]))
-        sys.exit(1)
-
     conn = MySQLdb.connect(host="localhost",
                            port=3306,
                            user=sys.argv[1],
@@ -16,8 +12,8 @@ if __name__ == '__main__':
                            db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
-    cur.execute(query, sys.argv[4])
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
