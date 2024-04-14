0f673fe5609a090d5e2cfb711f2bd0ff56a38fe4
HebaZaki <h_borhan@hotmail.com>
2024-04-14 22:31:26 +0200

    filter cities and prevent SQL injection

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""takes in name of a state as argument & lists all cities of that state"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    query = """SELECT cities.id, cities.name, states.name
+               FROM cities
+               JOIN states ON cities.state_id = states.id
+               WHERE states.name = %s
+               ORDER BY cities.id ASC"""
+    state_name = (sys.argv[4],)
+    cur.execute(query, state_name)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
