96abff8ec9924e46bf5d3e7dc54306d3b108a275
HebaZaki <h_borhan@hotmail.com>
2024-04-14 21:48:23 +0200

    my states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,11 +14,10 @@ if __name__ == '__main__':
                            user=sys.argv[1],
                            passwd=sys.argv[2],
                            db=sys.argv[3],
-                           state_name=sys.argv[4],
                            charset="utf8")
     cur = conn.cursor()
     query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
-    cur.execute(query)
+    cur.execute(query, sys.argv[4])
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
