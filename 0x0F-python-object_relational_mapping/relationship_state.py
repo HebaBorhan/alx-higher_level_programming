20b8c921f244c4b869ed3eb112e34b48bac96f96
HebaZaki <h_borhan@hotmail.com>
2024-04-15 12:05:30 +0200

    state class updated with relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class definition of a State and an instance Base = declarative_base()"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_city import Base, City
+
+
+class State(Base):
+    """state class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City", back_populates="state", cascade="all, delete")
