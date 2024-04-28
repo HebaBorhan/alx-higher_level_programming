a05be7b1726f6d078e40ca18e05f9d710938e83c
HebaZaki <h_borhan@hotmail.com>
2024-04-29 00:17:32 +0300

    POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email
+and displays the body of the response (decoded in utf-8)"""
+import urllib.request as request
+import urllib.parse as parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = parse.urlencode({'email': email}).encode()
+
+    with request.urlopen(url, data=data) as response:
+        decoded_body = response.read().decode("utf-8")
+        print(decoded_body)
