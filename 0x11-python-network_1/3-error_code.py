47e8f55eca02e8f940122e8a42f21103847cf208
HebaZaki <h_borhan@hotmail.com>
2024-04-29 01:35:38 +0300

    error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
             print(response.read().decode('utf-8'))
 
     except urllib.error.HTTPError as error:
-        print("Error code: ", error.code)
+        print("Error code: {}".format(error.code))
