6ef8243e7bea4e377260497ae4a1312ab083d3cf
HebaZaki <h_borhan@hotmail.com>
2024-04-29 01:31:31 +0300

    POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """sends a POST request to the passed URL with the email
-and displays the body of the response (decoded in utf-8)"""
+and displays body of the response (decoded in utf-8)"""
 import urllib.request as request
 import urllib.parse as parse
 import sys
