7f0e51e0e02181bd1931bb9fb167ddcf3e95f306
HebaZaki <h_borhan@hotmail.com>
2024-04-29 01:54:40 +0300

    status - requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status')
-    html = response.content
+    html = response.text
     print("Body response:")
     print("\t- type:", type(html))
     print("\t- content:", html)
