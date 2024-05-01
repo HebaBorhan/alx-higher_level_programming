db5244835a6e9f4a434dbd86ebeabeff28d0fd30
HebaZaki <h_borhan@hotmail.com>
2024-05-01 12:46:17 +0300

    body size - curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This Script takes in a URL, sends a request to it,
-# and displays the size of the body of the response.
-
+# This Script takes in a URL, sends a request to it, and displays the size of the body of the response.
 curl -s "$1" | wc -c
