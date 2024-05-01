76c4701f030f6ff22c5686d12dfc736ee913711f
HebaZaki <h_borhan@hotmail.com>
2024-05-01 12:59:01 +0300

    Get request - curl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This Script takes in a URL, sends a GET request to it, and displays the body of the response
-curl -s "$1" | grep -oP "(?<=^HTTP\/1\.1 200 OK\r\n\r\n).*"
+curl -sL "$1"
