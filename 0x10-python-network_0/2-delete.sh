6a31b1d625ab04a23153fcd5dd84cdfb0c29d90e
HebaZaki <h_borhan@hotmail.com>
2024-05-01 13:54:04 +0300

    Delete request - curl

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This Script sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# This Script sends DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
