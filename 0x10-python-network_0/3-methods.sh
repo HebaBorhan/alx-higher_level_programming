598000d531e76ab6aee4716933d4ac9c748b7b7b
HebaZaki <h_borhan@hotmail.com>
2024-05-01 14:11:52 +0300

    HTTP methods - curl

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This Script takes in a URL and displays all HTTP methods the server will accept.
+# This Script takes in URL and displays all HTTP methods the server will accept.
 curl -sI -X OPTIONS "$1" | awk '/Allow/{print $2}'
