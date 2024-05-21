a52c0ef9a2f73cabb005563fdce785d3a686cbb5
HebaZaki <h_borhan@hotmail.com>
2024-05-22 01:18:14 +0300

    Nodejs webpage content

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,11 +1,21 @@
 #!/usr/bin/node
 
-const fs = require('node:fs');
+const fs = require('fs');
+const request = require('request');
+
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-fs.writeFile(url, filePath, 'utf8', err => {
-  if (err) {
-    console.error(err);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
   }
+
+  fs.writeFile(filePath, body, 'utf8', err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
 });
